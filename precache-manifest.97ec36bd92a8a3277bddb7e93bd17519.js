87a545e6b0480dc7d51a09c49aee206fbda06070
SeanChao <SeanChao0804@gmail.com>
2020-04-24 12:30:26 +0000

    Deploying to gh-pages from  @ 59f4b60e93e054f01ad2b8e5c5eb54f8f30e6d10 🚀

---
+++ precache-manifest.97ec36bd92a8a3277bddb7e93bd17519.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c62d96c5d06f4c344306ea3bbcd090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdf77e671dd646601c70",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "64fb405ecb73a576c6b0",
+    "url": "/static/css/main.a97a02f9.chunk.css"
+  },
+  {
+    "revision": "bdf77e671dd646601c70",
+    "url": "/static/js/2.21ffa846.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.21ffa846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64fb405ecb73a576c6b0",
+    "url": "/static/js/main.3e41a2f4.chunk.js"
+  },
+  {
+    "revision": "4e74873627ea51303d4f",
+    "url": "/static/js/runtime-main.fb1b17d8.js"
+  }
+]);
