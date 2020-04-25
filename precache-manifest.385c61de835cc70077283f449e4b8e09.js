4af4990097a071d45574ec8dcc731a6451378660
SeanChao <SeanChao0804@gmail.com>
2020-04-25 02:35:50 +0000

    Deploying to gh-pages from  @ 89707875756a8d9666d70c9924ef17d1e6ab9c66 🚀

---
+++ precache-manifest.385c61de835cc70077283f449e4b8e09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f472434fc34483213af89c5ea95a054a",
+    "url": "/ChuanyiBot/index.html"
+  },
+  {
+    "revision": "bdf77e671dd646601c70",
+    "url": "/ChuanyiBot/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "e996a10420b5c6f58c44",
+    "url": "/ChuanyiBot/static/css/main.a97a02f9.chunk.css"
+  },
+  {
+    "revision": "bdf77e671dd646601c70",
+    "url": "/ChuanyiBot/static/js/2.21ffa846.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ChuanyiBot/static/js/2.21ffa846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e996a10420b5c6f58c44",
+    "url": "/ChuanyiBot/static/js/main.158a81d0.chunk.js"
+  },
+  {
+    "revision": "f2b2eedd3f97ad1571c0",
+    "url": "/ChuanyiBot/static/js/runtime-main.d7baecb5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97ec36bd92a8a3277bddb7e93bd17519.js"
+  "/ChuanyiBot/precache-manifest.385c61de835cc70077283f449e4b8e09.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ChuanyiBot/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
